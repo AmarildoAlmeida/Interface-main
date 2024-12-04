977f9ec47b8a4da2d7e61797579218492605863e
AmarildoAlmeida <amarildo.almeida.s@gmail.com>
2024-12-04 18:21:23 -0400

    Confg_Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
-import { router } from './routes';
 import GlobalStyles from './styles/globalStyles';
 import { Elements } from '@stripe/react-stripe-js';
 import AppProvider from './hooks'; // alteração
 import stripePromise from './config/stripeConfig';
 import { ThemeProvider } from 'styled-components';
 import { standardTheme } from './styles/themes/standard';
+import { Router } from './routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={standardTheme}>
       <AppProvider>
         <Elements stripe={stripePromise}>
-          <RouterProvider router={router} />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
         </Elements>
         <GlobalStyles />
         <ToastContainer autoClose={2000} theme="colored" />
